6a479bc36ff393c82b8122550bfe166eb2cb0b90
Dan Rusk <danrusk@thorntechnologies.com>
2018-10-01 21:27:09 -0400

    Uses fetch to get data

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
 "use strict";
 
-const data = {
-  metadata:{
-    count: 4,
-  },
-  data: [
-    {
-      title: "Star Wars Diner",
-      artist: "Edward hopper",
-      valueInDollars: 1000,
-    },
-    {
-      title: "Scream",
-      artist: "Munch",
-      valueInDollars: 10000,
-    },
-    {
-      title: "Colors",
-      artist: "Toddler",
-      valueInDollars: 1000000,
-    },
-    {
-      title: "My Art",
-      artist: "Dan Rusk",
-      valueInDollars: 1,
-    },
-  ]
-};
+fetch('https://api.myjson.com/bins/d3p74')
+  .then(response => response.json())
+  .then(data => {
+    const paintings = data.data;
+    paintings
+      .filter(filterExpensive)
+      .sort((a, b) => {
+        if(a.title < b.title){
+          return -1;
+        } else if (a.title === b.title){
+          return 0;
+        } else {
+          return 1;
+        }
+      })
+      .map(addArtistListItem);
 
-const paintings = data.data;
+    const totalValue = paintings.reduce(sumPaintingValues, 0);
+    console.log(`Total Value: ${totalValue}`);
+  });
 
+/**
+ * Adds the painting information to the html element with id of list
+ * @param painting a Painting object with {artist, title, valueInDollars}
+ * @returns {*} painting object that was passed in
+ */
 function addArtistListItem(painting) {
   const titleAndArtist = `${painting.title} by ${painting.artist}`;
   const list = document.getElementById("list");
@@ -40,24 +36,21 @@ function addArtistListItem(painting) {
   return painting;
 }
 
+/**
+ * Filters out paintings less than $2
+ * @param painting a Painting object with {artist, title, valueInDollars}
+ * @returns {boolean}
+ */
 function filterExpensive(painting){
   return painting.valueInDollars >= 2
 }
 
-paintings
-  .filter(filterExpensive)
-  .sort((a, b) => {
-    if(a.title < b.title){
-      return -1;
-    } else if (a.title === b.title){
-      return 0;
-    } else {
-      return 1;
-    }
-  })
-  .map(addArtistListItem);
-
+/**
+ * A reducer for totalling the painting values
+ * @param total
+ * @param painting
+ * @returns {*}
+ */
 const sumPaintingValues = (total, painting) => total + painting.valueInDollars;
-const totalValue = paintings.reduce(sumPaintingValues, 0);
-console.log(`Total Value: ${totalValue}`);
+
 
